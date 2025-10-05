89d63ebc20057cf72ee32d9b69bf57e09bffff00
KainTaimu <erwin.panergo1@gmail.com>
2025-10-05 00:14:22 -0600

    change: separate fps counter

---
+++ src/ui/fpscounter/FpsCounter.cs
@@ -0,0 +1,26 @@
+using System.Text;
+
+namespace SurvivorsGame.ui;
+
+public partial class FpsCounter : CanvasLayer
+{
+    [Export]
+    private Label _fps;
+
+    [Export]
+    private Label _processTime;
+    
+    [Export]
+    private Label _physicsTime;
+    
+    public override void _Process(double delta)
+    {
+        var fps = Performance.GetMonitor(Performance.Monitor.TimeFps);
+        var processTime = Performance.GetMonitor(Performance.Monitor.TimeProcess) * 1000;
+        var physicsTime = Performance.GetMonitor(Performance.Monitor.TimePhysicsProcess) * 1000;
+
+        _fps.Text = $"{fps:F2} FPS";
+        _processTime.Text = $"{processTime:F2} ms";
+        _physicsTime.Text = $"{physicsTime:F2} ms";
+    }
+}
