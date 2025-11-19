d4fee43ba460f415889bc7c41ea5cf48e5eb58cc
KainTaimu <erwin.panergo1@gmail.com>
2025-11-18 20:08:13 -0700

    change: crosshair inherits thread group change: crosshair uses _UnhandledInput instead of _Input

--- src/ui/hud/crosshair/Crosshair.cs
+++ src/ui/hud/crosshair/Crosshair.cs
@@ -43,7 +43,7 @@ public partial class Crosshair : Node2D
         ClampCrosshairToViewport();
     }
 
-    public override void _Input(InputEvent @event)
+    public override void _UnhandledInput(InputEvent @event)
     {
         if (@event is not InputEventMouseMotion motion)
             return;
