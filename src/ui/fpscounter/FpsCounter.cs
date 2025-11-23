beae9b837b5216ce740760239a0731a9dd0414f9
KainTaimu <erwin.panergo1@gmail.com>
2025-11-23 12:09:30 -0700

    add: DebugMenu addon
    https://github.com/godot-extended-libraries/godot-debug-menu

--- src/ui/fpscounter/FpsCounter.cs
+++ src/ui/fpscounter/FpsCounter.cs
@@ -1,24 +1,9 @@
 namespace SurvivorsGame.ui;
 
-public partial class FpsCounter : CanvasLayer
+public partial class FpsCounter : Node
 {
-    [Export]
-    private Label _fps;
-
-    [Export]
-    private Label _processTime;
-
-    [Export]
-    private Label _physicsTime;
-
-    public override void _Process(double delta)
+    public override void _Ready()
     {
-        var fps = Performance.GetMonitor(Performance.Monitor.TimeFps);
-        var processTime = Performance.GetMonitor(Performance.Monitor.TimeProcess) * 1000;
-        var physicsTime = Performance.GetMonitor(Performance.Monitor.TimePhysicsProcess) * 1000;
-
-        _fps.Text = $"FPS: {fps:F2}";
-        _processTime.Text = $"Process: {processTime:F2}ms";
-        _physicsTime.Text = $"Physics: {physicsTime:F2}ms";
+        GetNode("/root/DebugMenu").Set("style", 2);
     }
 }
