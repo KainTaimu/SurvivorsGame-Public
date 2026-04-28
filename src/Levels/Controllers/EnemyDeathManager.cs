4b855011424cd2c6374eaa5bd1ca31b63ac66409
Erwin Panergo <erwin.panergo1@gmail.com>
2026-04-28 11:29:28 -0600

    change: live death particle pool size changing
    change: tweak BloodKillParticles

--- src/Levels/Controllers/EnemyDeathManager.cs
+++ src/Levels/Controllers/EnemyDeathManager.cs
@@ -12,24 +12,70 @@ public partial class EnemyDeathManager : Node
 	private EntityComponentStore ComponentStore = null!;
 
 	[Export]
-	private int _maxActiveParticles = 100;
+	private int MaxActiveParticles
+	{
+		get;
+		set
+		{
+			UpdateParticles(value);
+			field = value;
+		}
+	} = 100;
 
 	private readonly Queue<GpuParticles2D> _activeParticles = [];
 	private readonly Queue<GpuParticles2D> _inactiveParticles = [];
 
-	public override void _Ready()
+	private ProcessModeEnum? _particlesOriginalProcessMode;
+
+	private void UpdateParticles(int maxParticles)
 	{
 		if (_enemyDeathParticlesScene is null)
 			return;
 
-		for (var i = 0; i < _maxActiveParticles; i++)
+		var particlesLeft =
+			maxParticles - _activeParticles.Count - _inactiveParticles.Count;
+
+		Logger.LogDebug($"start: {particlesLeft}");
+		while (particlesLeft < 0)
 		{
-			var gpu = _enemyDeathParticlesScene.Instantiate<GpuParticles2D>();
-			gpu.Hide();
-			gpu.Finished += gpu.Hide;
-			AddChild(gpu);
-			_inactiveParticles.Enqueue(gpu);
+			if (!_inactiveParticles.TryDequeue(out var particles))
+				break;
+			particles.QueueFree();
+			particlesLeft++;
 		}
+		while (particlesLeft < 0)
+		{
+			if (!_activeParticles.TryDequeue(out var particles))
+				break;
+			particles.QueueFree();
+			particlesLeft++;
+		}
+
+		for (var i = 0; i < particlesLeft; i++)
+		{
+			var particles =
+				_enemyDeathParticlesScene.Instantiate<GpuParticles2D>();
+			_particlesOriginalProcessMode ??= particles.ProcessMode;
+
+			particles.Finished += () =>
+			{
+				if (!IsInstanceValid(particles))
+					return;
+				particles.Hide();
+				DisableParticles(particles);
+			};
+			DisableParticles(particles);
+			AddChild(particles);
+			_inactiveParticles.Enqueue(particles);
+			Logger.LogDebug(
+				$"inactive({_inactiveParticles.Count}): {particlesLeft}"
+			);
+		}
+	}
+
+	public override void _Ready()
+	{
+		UpdateParticles(MaxActiveParticles);
 	}
 
 	public override void _Process(double delta)
@@ -50,17 +96,33 @@ public partial class EnemyDeathManager : Node
 		}
 		ComponentStore.UnregisterEntity(id);
 
-		if (_activeParticles.Count >= _maxActiveParticles)
+		if (MaxActiveParticles <= 0)
+			return;
+		if (_activeParticles.Count >= MaxActiveParticles)
 		{
 			var off = _activeParticles.Dequeue();
 			off.Hide();
 			_inactiveParticles.Enqueue(off);
 		}
 
-		var gpu = _inactiveParticles.Dequeue();
-		gpu.GlobalPosition = pos.Position;
-		gpu.Show();
-		gpu.Restart();
-		_activeParticles.Enqueue(gpu);
+		var particles = _inactiveParticles.Dequeue();
+		EnableParticles(particles, pos.Position);
+
+		_activeParticles.Enqueue(particles);
+	}
+
+	private void EnableParticles(GpuParticles2D particles, Vector2 position)
+	{
+		particles.GlobalPosition = position;
+		particles.Show();
+		particles.Restart();
+		particles.ProcessMode =
+			_particlesOriginalProcessMode ?? ProcessModeEnum.Inherit;
+	}
+
+	private void DisableParticles(GpuParticles2D particles)
+	{
+		particles.Hide();
+		particles.ProcessMode = ProcessModeEnum.Disabled;
 	}
 }
