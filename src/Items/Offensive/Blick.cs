61b057dd60444c10171a20c4a83692d09e178387
Erwin Panergo <erwin.panergo1@gmail.com>
2026-05-08 16:22:33 -0600

    add: blicky

---
+++ src/Items/Offensive/Blick.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Game.Core.ECS;
+
+namespace Game.Items.Offensive;
+
+public partial class Blick : Firearm
+{
+	public override void _Ready()
+	{
+		base._Ready();
+		OnAttack += ApplyCameraRecoil;
+	}
+
+	public override void _Process(double delta)
+	{
+		FireCooldown -= delta;
+		if (AttackActionString is null)
+			return;
+
+		if (Input.IsActionPressed(InputMapNames.WeaponReload))
+		{
+			Reload();
+			return;
+		}
+
+		if (!Input.IsActionPressed(AttackActionString))
+			return;
+
+		Attack();
+	}
+
+	protected override void HandleHitECS(int id)
+	{
+		if (!ComponentStore.GetComponent<PositionComponent>(id, out var pos))
+			return;
+		var knockback = Stats.Additional.GetValueOrDefault("Knockback").AsSingle();
+		var knockbackVector = Player.GlobalPosition.DirectionTo(pos.Position);
+		knockbackVector *= knockback;
+
+		ComponentStore.SetComponent(id, new PositionComponent(pos.Position + knockbackVector));
+	}
+}
