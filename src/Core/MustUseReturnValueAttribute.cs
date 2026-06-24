38b0aebabcdcefdf9ddad286ebe741968259108d
Erwin Panergo <erwin.panergo1@gmail.com>
2026-06-24 00:12:38 -0600

    change: remove redundant packages

--- src/Core/MustUseReturnValueAttribute.cs
+++
@@ -1,4 +0,0 @@
-[AttributeUsage(
-	AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Struct
-)]
-internal class MustUseReturnValueAttribute : Attribute { }
