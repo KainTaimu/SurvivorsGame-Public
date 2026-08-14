8d01b2dd465da5d9d2e258a10bd82fa59349f160
Erwin Panergo <erwin.panergo1@gmail.com>
2026-08-14 17:23:26 -0600

    change: use godot-mono

--- run_release.sh
+++ run_release.sh
@@ -1,2 +1,2 @@
-godot-mono-beta --path . --export-release Linux exports/linux/linux.x86_64 --headless
+godot-mono --path . --export-release Linux exports/linux/linux.x86_64 --headless
 gamemoderun ./exports/linux/linux.x86_64
