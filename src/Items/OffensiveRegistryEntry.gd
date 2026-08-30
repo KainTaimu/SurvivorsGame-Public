5e82aa5f49961f06cf45aabe03bc7e9aec393bfd
Erwin Panergo <erwin.panergo1@gmail.com>
2026-08-30 06:59:00 -0600

    change: move item properties to registry entry

--- src/Items/OffensiveRegistryEntry.gd
+++ src/Items/OffensiveRegistryEntry.gd
@@ -1,6 +1,7 @@
 class_name OffensiveRegistryEntry
 extends Resource
 
+@export var properties: BaseItemProperties
 @export var scene: PackedScene
 ## Lower is rarer
 @export_range(0, 1, 0.001) var spawn_weight: float
